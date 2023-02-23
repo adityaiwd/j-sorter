a3db196a2c3be19f700b0c3be42464e459591aea
Wicak <49396531+adityaiwd@users.noreply.github.com>
2023-02-23 21:00:12 +0700

    [ADD] new member images and new status options (#6)

--- src/helper/getGenerationOptions.js
+++ src/helper/getGenerationOptions.js
@@ -1,12 +1,41 @@
-import { members } from "../membersData"
-import { uniqBy } from "lodash"
-import { ACTIVE_STATUS, GRADUATED_STATUS } from '../../constants';
+import { members } from '../membersData';
+import { uniqBy } from 'lodash';
+import { ACTIVE_STATUS, GRADUATED_STATUS, MEMBERS_ONLY_STATUS, TRAINEE_ONLY_STATUS } from '../../constants';
 
-export const getGenerationOptions = (memberStatus) => {
-    if(memberStatus === ACTIVE_STATUS){
-        return uniqBy(members.filter(member => !member.graduated).map(member => ({ value: member.generation, label: `Gen ${member.generation}` })), 'value')
-    } else if (memberStatus === GRADUATED_STATUS) {
-        return uniqBy(members.filter(member => member.graduated).map(member => ({ value: member.generation, label: `Gen ${member.generation}` })), 'value')
-    }
-    return uniqBy(members.map(member => ({ value: member.generation, label: `Gen ${member.generation}` })), 'value')
-}
+export const getGenerationOptions = memberStatus => {
+  switch (memberStatus) {
+    case ACTIVE_STATUS:
+      return uniqBy(
+        members
+          .filter(member => !member.graduated)
+          .map(member => ({ value: member.generation, label: `Gen ${member.generation}` })),
+        'value',
+      );
+    case MEMBERS_ONLY_STATUS:
+      return uniqBy(
+        members
+          .filter(member => !member.graduated && !member.trainee)
+          .map(member => ({ value: member.generation, label: `Gen ${member.generation}` })),
+        'value',
+      );
+    case TRAINEE_ONLY_STATUS:
+      return uniqBy(
+        members
+          .filter(member => !member.graduated && member.trainee)
+          .map(member => ({ value: member.generation, label: `Gen ${member.generation}` })),
+        'value',
+      );
+    case GRADUATED_STATUS:
+      return uniqBy(
+        members
+          .filter(member => member.graduated)
+          .map(member => ({ value: member.generation, label: `Gen ${member.generation}` })),
+        'value',
+      );
+    default:
+      return uniqBy(
+        members.map(member => ({ value: member.generation, label: `Gen ${member.generation}` })),
+        'value',
+      );
+  }
+};
